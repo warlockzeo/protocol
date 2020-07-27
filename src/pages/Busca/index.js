1fda84a03b4c582ffb99cf89d1ab7bdb1171b09c
warlockzeo <warlockzeo@gmail.com>
2020-07-27 07:07:59 +0100

    Estrutura de Busca pronta

--- src/pages/Busca/index.js
+++ src/pages/Busca/index.js
@@ -1,9 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Row, Col, Button } from 'react-bootstrap';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+const protocolos = [1, 2, 3, 4, 5];
+
+const schema = Yup.object().shape({
+  search: Yup.string().required('Precisa informar um protocolo para busca'),
+});
 
 const Busca = () => {
-  return <h1>Busca de protocolo</h1>;
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const onSubmit = () => {
+    console.log('busca');
+  };
+
+  const onChangeField = () => {
+    setErrorMessage('');
+  };
+
+  return (
+    <>
+      <h1>Busca de protocolo</h1>
+      {errorMessage ? (
+        <Alert variant='danger' className='text-center'>
+          Nenhum protocolo encontrado para esta busca
+        </Alert>
+      ) : (
+        <Form onSubmit={onSubmit} schema={schema}>
+          <Row>
+            <Col md={6}>
+              <Input
+                className='form-control'
+                type='text'
+                name='login'
+                id='login'
+                placeholder='Login'
+                onChange={onChangeField}
+                onFocus={onChangeField}
+              />
+              {errorMessage &&
+                errorMessage.toLocaleLowerCase().search('login') > -1 && (
+                  <span>{errorMessage}</span>
+                )}
+            </Col>
+            <Col md={6}>
+              <Input
+                className='form-control'
+                type='password'
+                name='senha'
+                id='senha'
+                placeholder='Senha'
+                onChange={onChangeField}
+                onFocus={onChangeField}
+              />
+              {errorMessage &&
+                errorMessage.toLocaleLowerCase().search('passe') > -1 && (
+                  <span>{errorMessage}</span>
+                )}
+            </Col>
+          </Row>
+          <Button type='submit' className='form-control' color='danger'>
+            Submit
+          </Button>
+        </Form>
+      )}
+    </>
+  );
 };
 
 export default Busca;
